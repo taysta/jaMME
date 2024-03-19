195a7483166d53ea8622a146000736dc02139757
Tom <tomarrowtomarrow@hotmail.com>
2024-03-19 12:48:26 +0100

    Better handle cut off json strings.

--- codemp/cgame/cg_demos.c
+++ codemp/cgame/cg_demos.c
@@ -1218,6 +1218,10 @@ const char* simpleGetJSONValueForKey(const char* json, const char* key, int dept
 					while (*jsonFoundMaybe && (*jsonFoundMaybe == '\t' || *jsonFoundMaybe == '\n' || *jsonFoundMaybe == '\r' || *jsonFoundMaybe == ' ')) {
 						jsonFoundMaybe++; // fast forward through whitespaces
 					}
+					if (!*jsonFoundMaybe) {
+						jsonHere++; // Error.
+						continue;
+					}
 					if (*jsonFoundMaybe == '{' || *jsonFoundMaybe == '[') {
 						jsonHere++; // Unsupported. Move on.
 						continue;
